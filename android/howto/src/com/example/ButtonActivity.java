5caf0b827911183280afc2a625d28e2c1251459f
Kannan Chandrasekaran <kannanc@ck-dell.(none)>
2011-12-26 19:23:43 -0800

    * Modified the main.xml to use a combination of scroll and linear layouts to add demo buttons * Added a new ButtonActivity to demo the button click functionality and added  a home button to retrn to the main activity * Added  the activities to the manifest file. * Added logic to programatically launch the ButtonActivity intent in the MyActivity * Ignored the local.properties file from VCS

---
+++ android/howto/src/com/example/ButtonActivity.java
@@ -0,0 +1,55 @@
+package com.example;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
+/**
+ * A Simple activity to demonstrate button click.
+ *
+ * @author http://kannanchandrasekaran.com
+ */
+public class ButtonActivity extends Activity {
+    /**
+     * Go to the Main Demo activity
+     */
+    Button m_home;
+    /**
+     * Demo button
+     */
+    Button m_button;
+    Random m_rand;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.buttonactivity);
+        m_button = (Button) findViewById(R.id.button);
+        m_home = (Button) findViewById(R.id.home);
+        m_rand = new Random();
+    }
+
+    /**
+     * Onclick Handler. Gets called when the demo button is clicked.
+     * Sets the text property of the demo button with a random number.
+     *
+     * @param view
+     */
+    public void clickHandler(View view) {
+        m_button.setText("Setting random no to : " + m_rand.nextDouble());
+    }
+
+    /**
+     * Go to the main activityPage
+     *
+     * @param view
+     */
+    public void goHome(View view) {
+        Intent home = new Intent(this, MyActivity.class);
+        startActivity(home);
+    }
+}
--- android/howto/src/com/example/MyActivity.java
+++ android/howto/src/com/example/MyActivity.java
@@ -1,18 +1,18 @@
 package com.example;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
 
-import java.util.Date;
-
-public class MyActivity extends Activity implements RadioGroup.OnCheckedChangeListener {
-    TextView m_label;
-    Button m_button;
-    ImageView m_iv;
-    EditText m_et;
-    RadioGroup m_rg;
+/**
+ * Main demo activity page intended to demo the individual android components.
+ *
+ * @author http://kannanchandrasekaran.com
+ */
+public class MyActivity extends Activity {
+    Button m_demoBtn;
 
     /**
      * Called when the activity is first created.
@@ -21,31 +21,17 @@ public class MyActivity extends Activity implements RadioGroup.OnCheckedChangeLi
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        m_label = (TextView) findViewById(R.id.label);
-//        m_button = (Button) findViewById(R.id.button);
-//        m_iv = (ImageView) findViewById(R.id.iv);
-//        m_et = (EditText) findViewById(R.id.et);
-//        m_rg = (RadioGroup) findViewById(R.id.rg);
-//        m_rg.setOnCheckedChangeListener(this);
+
+        m_demoBtn = (Button) findViewById(R.id.btnDemo);
     }
 
-    public void onClickHandler(View view) {
-        updateTime();
-    }
-
-    private void updateTime() {
-        m_button.setText(new Date().toString());
-    }
-
-
-    @Override
-    public void onCheckedChanged(RadioGroup radioGroup, int i) {
-        int checkedButton = radioGroup.getCheckedRadioButtonId();
-        System.out.println("CheckedButton: :" + checkedButton);
-        System.out.println("i: :" + i);
-        RadioButton button = (RadioButton) findViewById(checkedButton);
-        if (button != null) {
-            m_label.setText(button.getText());
-        }
+    /**
+     * Start the {@code ButtonActivity}
+     *
+     * @param view
+     */
+    public void onClickButtonDemoHandler(View view) {
+        Intent btnDemoIntent = new Intent(this, ButtonActivity.class);
+        startActivity(btnDemoIntent);
     }
 }
