760d4da1faee730c6dc8d3adac56ebe921a089ce
kannan <kannan email>
2012-04-03 07:38:47 -0700

    * Modified the WebViewDemo to allow for customization of WebViewClient, WebChromeClient and custom Javascript interface

--- android/howto/src/com/example/WebViewDemo.java
+++ android/howto/src/com/example/WebViewDemo.java
@@ -6,7 +6,10 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.webkit.*;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 /**
@@ -24,8 +27,7 @@ public class WebViewDemo extends Activity {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.browser_demo);
       WebView wv = (WebView) findViewById(R.id.browser1);
-      loadResource(wv, URL_TO_LOAD);
-//      loadResource(wv, LOCAL_RESOURCE);
+      loadResource(wv, LOCAL_RESOURCE);
    }
 
 
@@ -35,66 +37,64 @@ public class WebViewDemo extends Activity {
       wv.setWebChromeClient(new CustomChromeclient());
       wv.setWebViewClient(new CustomWebViewclient(this));
       wv.addJavascriptInterface(new JavaScriptInterface(wv), "JSI");
-      wv.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
    }
 
-   private class CustomChromeclient extends WebChromeClient {
-      @Override
-      public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
-         AlertDialog dialog = new AlertDialog.Builder(view.getContext()).
-                 setMessage(message).
-                 setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                       //do nothing
-                    }
-                 }).create();
-         dialog.show();
-         result.confirm();
-         return true;
-      }
+private class CustomChromeclient extends WebChromeClient {
+   @Override
+   public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+      AlertDialog dialog = new AlertDialog.Builder(view.getContext()).
+              setMessage(message).
+              setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                 @Override
+                 public void onClick(DialogInterface dialog, int which) {
+                    //do nothing
+                 }
+              }).create();
+      dialog.show();
+      result.confirm();
+      return true;
+   }
+}
+
+
+private static class CustomWebViewclient extends WebViewClient {
+   private static int refreshCount;
+
+   private Context m_context;
+   private long m_start;
+
+   private CustomWebViewclient(Context context) {
+      m_context = context;
    }
 
-
-   private static class CustomWebViewclient extends WebViewClient {
-      private static int refreshCount;
-
-      private Context m_context;
-      private long m_start;
-
-      private CustomWebViewclient(Context context) {
-         m_context = context;
-      }
-
-      @Override
-      public boolean shouldOverrideUrlLoading(WebView view, String url) {
-         return false;
-      }
-
-
-      @Override
-      public void onPageStarted(WebView view, String url, Bitmap favicon) {
-         m_start = System.currentTimeMillis();
-         refreshCount++;
-      }
-
-      @Override
-      public void onPageFinished(WebView view, String url) {
-         long interval = System.currentTimeMillis() - m_start;
-         Toast.makeText(m_context, "Loaded this webpage [" + refreshCount + "] " +
-                 "times in [" + interval + "] ms", Toast.LENGTH_SHORT).show();
-      }
+   @Override
+   public boolean shouldOverrideUrlLoading(WebView view, String url) {
+      return false;
    }
 
-   private class JavaScriptInterface {
-      private final WebView m_wv;
-
-      public JavaScriptInterface(WebView wv) {
-         m_wv = wv;
-      }
-
-      public void reload() {
-         m_wv.reload();
-      }
+   @Override
+   public void onPageStarted(WebView view, String url, Bitmap favicon) {
+      m_start = System.currentTimeMillis();
+      refreshCount++;
    }
+
+   @Override
+   public void onPageFinished(WebView view, String url) {
+      long interval = System.currentTimeMillis() - m_start;
+      Toast.makeText(m_context, "Loaded this webpage [" + refreshCount + "] " +
+              "times in [" + interval + "] ms", Toast.LENGTH_SHORT).show();
+   }
+}
+
+private class JavaScriptInterface {
+   private final WebView m_wv;
+
+   public JavaScriptInterface(WebView wv) {
+      m_wv = wv;
+   }
+
+   public void reload() {
+      m_wv.reload();
+   }
+}
 }
